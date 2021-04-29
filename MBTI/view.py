c1233d20c86e283275035b6058175c5a8ebb4721
otch80 <minkk1224@gmail.com>
2021-04-29 14:39:41 +0900

    DJANGO 초기 설정 완료 (누락부분)

---
+++ MBTI/view.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def login(request):
+    print(request)
+    return render(request, "MBTI/login.html")
+
+def results(request, question_id):
+    response = "You're looking at the results of question %s."
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    return HttpResponse("You're voting on question %s." % question_id)
