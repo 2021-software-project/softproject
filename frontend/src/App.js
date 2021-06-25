440ef828bbcaab2604ed399cfb6834c7d2d162bd
yeonui-0626 <lye0626@gmail.com>
2021-06-25 14:37:20 +0900

    react,django 연동

---
+++ frontend/src/App.js
@@ -0,0 +1,26 @@
+import React, {Suspense} from 'react';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import './App.css';
+import NavBar from './components/NavBar/NavBar';	// 추가
+import LoginPage from './components/UserPage/LoginPage';	// 추가
+import SignupPage from './components/UserPage/SignupPage';	// 추가
+import Home from "./components/home";
+function App() {
+  return (
+    <Router>
+      <Suspense fallback={(<div>...</div>)}>
+        <NavBar />
+          <div className="App">
+            <Switch>
+              <Route exact path="/login/" component={LoginPage}></Route>	// 추가
+              <Route exact path="/signup/" component={SignupPage}></Route>	// 추가
+              <Route exact path="/" component={Home}></Route>	// 추가
+            </Switch>
+          </div>
+      </Suspense>
+    </Router>
+    );
+}
+
+export default App;
---
+++ user/admin.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.admin import UserAdmin
+from .forms import CustomUserChangeForm, CustomUserCreationForm
+from .models import CustomUser
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ['email']
