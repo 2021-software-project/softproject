79f2682cda21ec845fc83a38e79441394187b18f
Song-hyunji <80735829+Song-hyunji@users.noreply.github.com>
2021-07-30 01:39:44 +0900

    index.js 파일 업로드
    리덕스 스토어 구축하여 연동한 index.js

---
+++ frontend/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+//import Routes from './Routes';
+import { createStore } from 'redux';  //리덕스를 위해 추가함
+import { Provider } from 'react-redux';
+import rootReducer from './store/modules/rootReducer';
+
+const store = createStore(rootReducer);
+console.log(store.getState());
+
+//Provider로 store를 넣어서 App 을 감싸게 되면 우리가 렌더링하는 그 어떤 컴포넌트던지 리덕스 스토어에 접근가능
+//여기에서 index.js로 ReactDOM에 의해 렌더링됨
+ReactDOM.render(
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>,
+    //<Routes />,
+  document.getElementById('root')
+);
+
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
