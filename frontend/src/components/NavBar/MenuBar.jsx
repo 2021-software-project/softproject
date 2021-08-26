0a83d20d7b9180f7019c15fc3cdde637b9cd1c90
kimmengsung <minjigongjoo98@gmail.com>
2021-08-27 00:02:17 +0900

    메뉴바, footer(하단바) css 설정

--- frontend/src/components/NavBar/MenuBar.jsx
+++ frontend/src/components/NavBar/MenuBar.jsx
@@ -2,23 +2,44 @@ import React from 'react';
 import {Link} from "react-router-dom";
 import MenuBarItem from "./MenuBarItem";
 import "./Bar.css"
+import "../../css/style2.css";
+
 
 function MenuBar(){
-    const menus = [
-        {name : "MBTI별 추천", path:"/Mbti_rcm"},
-        {name : "개인별 추천", path:"/Personal_rcm"},
-        {name : "알바평가", path:"/Alba_rating"},
-    ];
+    //const menus = [
+      //  {name : "MBTI별 추천", path:"/Mbti_rcm"},
+        //{name : "개인별 추천", path:"/Personal_rcm"},
+     //   {name : "알바평가", path:"/Alba_rating"},
+  //  ];
     return(
-        <div className="menubar">
-            {menus.map((menu,index)=>{
-                return (
-                    <Link to={menu.path} key={index}>
-                        <MenuBarItem menu={menu}/>
-                    </Link>
-                );
-            })}
-        </div>
+       // <div className="menubar">
+          //  {menus.map((menu,index)=>{
+             //   return (
+             //       <Link to={menu.path} key={index}>
+                        //<MenuBarItem menu={menu}/>
+             //       </Link>
+               // );
+          //  })}
+
+
+					<div id="sidebar">
+						<div class="inner">
+
+								<nav id="menu">
+									<header class="major">
+										<h2>Menu</h2>
+									</header>
+									<ul>
+										<li><a href="/Mbti_rcm">MBTI별 추천</a></li>
+										<li><a href="/Personal_rcm">개인별 추천</a></li>
+										<li><a href="/Alba_rating">알바평가</a></li>
+
+									</ul>
+								</nav>
+
+						</div>
+					</div>
+       // </div>
     )
 }
 
--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import Axios from 'axios';
 import { Menu } from 'antd';
 import styled from 'styled-components';
+import "../../css/styles.css";
 
 const NavList = styled.div`
   display: flex;
@@ -30,43 +31,60 @@ function NavBar() {
     const handleLogout = () => {
         let token = localStorage.getItem('token')
 
-        Axios.post('http://127.0.0.1:8000/user/auth/logout/', token)
+        Axios.post('http://localhost:8000/user/auth/logout/', token)
           .then(res => {
             localStorage.clear()
             // 사용하려면 App.js에서 /로 라우팅해야 한다
             window.location.replace('/')
           });
     }
+    const onClink=(e)=>{
 
+    }
     return(
-        <div>
-            <NavList>
-                <Menu>
-                    <Link to="/main">
-                        메인 페이지
-                    </Link>
-                    { auth ?
-                        <a onClick={handleLogout}>
-                            로그아웃
-                        </a>
-                        :
-                            <Link to="/login">
-                                로그인
-                            </Link>
-                    }
-                    { auth ?
-                        <Link to="/mypage">
-                            마이페이지
-                        </Link>
-                        :
-                        <Link to="/signup">
-                            회원가입
-                        </Link>
-                    }
+        <div id="page-top">
 
-                </Menu>
-            </NavList>
+            <nav className="navbar navbar-expand-lg bg-secondary text-uppercase" id="mainNav">
+                <div className="container">
+                    <a className="navbar-brand" href="#page-top">Start Bootstrap</a>
+                    <button className="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded"
+                            type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive"
+                            aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                        Menu
+                        <i className="fas fa-bars"></i>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarResponsive">
+                        <ul className="navbar-nav ms-auto">
+                            <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded"
+                                                                     href="/main">메인 페이지</a></li>
+                            { auth ?
+                                <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded"
+                                                                     onClick={handleLogout} href="#logout">로그아웃</a></li>
+                                :
+                                <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded"
+                                                                     href="/login">로그인</a></li>
+                            }
+
+                            { auth ?
+                                <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded"
+                                                                     href="/mypage">마이페이지</a></li>
+                                :
+                                <li className="nav-item mx-0 mx-lg-1"><a className="nav-link py-3 px-0 px-lg-3 rounded"
+                                                                     href="/signup">회원가입</a></li>
+                            }
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+
+
+
+
+
+
+
         </div>
+
     )
 }
 
